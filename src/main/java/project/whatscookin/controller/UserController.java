1a287facd4cbb983fe23c3900679c9fa0a20afe5
Gage Schmidt <g.harakiri@gmail.com>
2019-02-17 08:05:41 -0600

    Began adding a User class, and Authentication

---
+++ src/main/java/project/whatscookin/controller/UserController.java
@@ -0,0 +1,15 @@
+/*package project.whatscookin.controller;
+
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/hello")
+public abstract class UserController implements WebSecurityConfigurer {
+    @RequestMapping(value="/hello")
+
+
+}
+*/
